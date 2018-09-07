d53ccc901dd022c518baab5cda740e9083c6c7cf
O S <vystoya@gmail.com>
2018-09-07 16:36:41 +0300

    create a restful web service in netbeans

---
+++ build/generated-sources/ap-source-output/com/playerentity/Player_.java
@@ -0,0 +1,17 @@
+package com.playerentity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-07T15:47:49")
+@StaticMetamodel(Player.class)
+public class Player_ { 
+
+    public static volatile SingularAttribute<Player, String> firstname;
+    public static volatile SingularAttribute<Player, Integer> jerseynumber;
+    public static volatile SingularAttribute<Player, String> lastspokenwords;
+    public static volatile SingularAttribute<Player, Integer> id;
+    public static volatile SingularAttribute<Player, String> lastname;
+
+}
---
+++ build/generated-sources/ap-source-output/com/playerentity/Team_.java
@@ -0,0 +1,15 @@
+package com.playerentity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-07T15:47:49")
+@StaticMetamodel(Team.class)
+public class Team_ { 
+
+    public static volatile SingularAttribute<Team, Integer> teamId;
+    public static volatile SingularAttribute<Team, String> league;
+    public static volatile SingularAttribute<Team, String> teamname;
+
+}
